b3329871a39f30fb8ecdc75e707ebf410899b840
Georgia <geo02zarn@gmail.com>
2018-03-15 15:02:31 +0100

    Created activities for bayesian and particle filters

---
+++ app/src/main/java/com/example/georgia/sps_localization/BayesFilter.java
@@ -0,0 +1,13 @@
+package com.example.georgia.sps_localization;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BayesFilter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bayes_filter);
+    }
+}
